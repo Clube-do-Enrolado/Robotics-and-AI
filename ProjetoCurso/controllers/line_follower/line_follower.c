d5aabb250ad521a1c84d21f3340be0ec1fcc42fc
vi_acosta <55991392+VitorAcosta@users.noreply.github.com>
2021-10-31 19:40:18 -0300

    Robot following line
    Initial version implemented with ground IR sensors in E-Puck robot.

---
+++ ProjetoCurso/controllers/line_follower/line_follower.c
@@ -0,0 +1,201 @@
+/*
+ * File:          SegueLinha.c
+ * Date:          31/10/2021
+ * Description:   Algoritmo para que o robô siga a linha de um percurso.
+ * Author:        Vitor Acosta
+ * Modifications: 
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <webots/robot.h>
+#include <webots/motor.h>
+#include <webots/distance_sensor.h>
+#include <webots/supervisor.h>
+
+
+// Velocidade máxima, em rad/s, que os
+// motores do E-Puck operam.
+#define MAX_SPEED 6.28
+
+// Percentual de velocidade que será
+// aplicado aos motores
+#define MOTOR_INTENSITY 0.25
+
+#define IR_THRESHOLD 0.1
+
+#define IR_TRACK_VALUE 6
+
+// Motores do E-Puck
+static WbDeviceTag left_motor, right_motor;
+
+// Sensores de proximidade
+WbDeviceTag ps[8];
+// Leitura da saída dos sensores
+double ps_values[8];
+
+// Sensores IR (infra-vermelho) para detectar
+// a linha do percurso.
+static WbDeviceTag left_ground, right_ground;
+// Leitura da saída dos sensores
+static double left_ground_value, right_ground_value;
+
+// Tempo da simulação
+int time_step;
+
+
+/*
+ * Adquire o tempo da simulação.
+ */
+int get_time_step() {
+  static int time_step = -1;
+  if (time_step == -1)
+    time_step = (int)wb_robot_get_basic_time_step();
+  return time_step;
+}
+
+void step()
+{
+  if (wb_robot_step(time_step) == -1){
+    wb_robot_cleanup();
+    exit(EXIT_SUCCESS);
+  }
+}
+
+//==========================//
+//          MOTORES         //
+//==========================//
+
+/*
+ * Função que define velocidade 0 à ambos motores.
+ * Fazendo o robô parar.
+ */
+void motor_stop(){
+  wb_motor_set_velocity(left_motor, 0);
+  wb_motor_set_velocity(right_motor, 0);
+}
+
+/*
+ * Função que define velocidade máxima à ambos motores.
+ * Fazendo que o robô vá para frente.
+ */
+void motor_move_forward(){
+  wb_motor_set_velocity(left_motor, MOTOR_INTENSITY*MAX_SPEED);
+  wb_motor_set_velocity(right_motor, MOTOR_INTENSITY*MAX_SPEED);
+}
+
+/*
+ * Função que define velocidade negativa máxima ao motor esquerdo
+ * e vlocidade positiva máxima ao motor direito.
+ * Fazendo que o robô rode (em seu próprio eixo) para a esqueda.
+ */
+void motor_rotate_left(){
+  wb_motor_set_velocity(left_motor, -MOTOR_INTENSITY*MAX_SPEED);
+  wb_motor_set_velocity(right_motor, MOTOR_INTENSITY*MAX_SPEED);
+}
+
+/*
+ * Função que define velocidade positiva máxima ao motor esquerdo
+ * e vlocidade negativa máxima ao motor direito.
+ * Fazendo que o robô rode (em seu próprio eixo) para a direita.
+ */
+void motor_rotate_right(){
+  wb_motor_set_velocity(left_motor, MOTOR_INTENSITY*MAX_SPEED);
+  wb_motor_set_velocity(right_motor, -MOTOR_INTENSITY*MAX_SPEED);
+}
+
+
+/////////////////////
+//       ROBÔ      //
+/////////////////////
+
+/*
+ * Método de inicialização do robô.
+ * Define os valores de inicialização dos sensores,
+ * motores e tempo de simulação.
+ */
+void init(){
+
+  time_step = get_time_step();
+  
+  // Nome dos sensores de proximidade
+  char ps_names[8][4] ={
+    "ps0", "ps1", "ps2", "ps3",
+    "ps4", "ps5", "ps6", "ps7"
+  };  
+
+  // Inicialização dos sensores.
+  for (int i = 0; i < 8 ; i++) {
+    // Liga sensores de proximidade
+    ps[i] = wb_robot_get_device(ps_names[i]);
+            
+    // Ativa todos os sensores, os resultados serão coletados
+    // periódicamente em milissegundos TIME_STEP. 
+    wb_distance_sensor_enable(ps[i], time_step);
+  }
+  
+  // Inicialização dos motores das rodas esquerda e direita, respectivamente.
+  left_motor = wb_robot_get_device("left wheel motor");
+  right_motor = wb_robot_get_device("right wheel motor");
+  wb_motor_set_position(left_motor, INFINITY);
+  wb_motor_set_position(right_motor, INFINITY);
+  wb_motor_set_velocity(left_motor, 0.0);
+  wb_motor_set_velocity(right_motor, 0.0);
+  
+  // Inicializa os sensores de IR do solo.
+  left_ground = wb_robot_get_device("IR0");
+  wb_distance_sensor_enable(left_ground, time_step);
+  right_ground = wb_robot_get_device("IR1");
+  wb_distance_sensor_enable(right_ground,time_step);
+  
+}
+
+
+int main(int argc, char **argv) {
+    
+  wb_robot_init();
+  
+  init();
+  
+  while (true) {
+    // Obtenção dos valores indicados nos
+    // sensores de proximidade
+    for (int i = 0; i < 8; i++)
+      ps_values[i] = wb_distance_sensor_get_value(ps[i]);
+      
+    bool right_obstacle =  ps_values[0] > 140.0 ||
+                          ps_values[1] > 140.0;
+    bool left_obstacle =   ps_values[6] > 140.0 ||
+                          ps_values[7] > 140.0;
+      
+    
+    left_ground_value = wb_distance_sensor_get_value(left_ground);
+    right_ground_value = wb_distance_sensor_get_value(right_ground);
+    
+    printf("\nGround Sensors Values: \n(Left)%4.4f\n(Right)%4.4f\n",
+    left_ground_value,
+    right_ground_value);
+    
+    motor_move_forward();
+    
+    if( (left_ground_value > right_ground_value) &&
+        (IR_TRACK_VALUE - IR_THRESHOLD < left_ground_value && left_ground_value < 15)){
+      motor_rotate_left();
+    }
+    else if( (right_ground_value > left_ground_value) &&
+        (IR_TRACK_VALUE - IR_THRESHOLD < right_ground_value && left_ground_value < 15)){
+      motor_rotate_right();
+    }
+
+    /*if (GroundD_Valor<5 || GroundE_Valor<5){
+      if (GroundD_Valor<5) Velocidades[LEFT] = -Velocidades[LEFT]*1;
+      else if (GroundE_Valor<5) Velocidades[RIGHT] = -Velocidades[RIGHT]*1;
+    }
+    wb_motor_set_velocity(motorE, Velocidades[LEFT]);
+    wb_motor_set_velocity(motorD, Velocidades[RIGHT]); 
+    */
+    step();
+  }
+}
