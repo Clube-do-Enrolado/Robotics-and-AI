99c2ac7a3cce66983da15104ede42bac2d180aaa
vi_acosta <55991392+VitorAcosta@users.noreply.github.com>
2021-11-08 22:39:00 -0300

    Ajuste nas velocidades máximas e limpeza de código
    Limitação da velocidade máxima em curvas e retas.
    Implementação de limites nas condições que avaliam a leitura do sensor para a tomada de decisão.
    Limpeza de comentários e códigos obsoletos do algoritmo.

--- ProjetoCurso/controllers/line_follower/line_follower.c
+++ ProjetoCurso/controllers/line_follower/line_follower.c
@@ -24,13 +24,16 @@
 #define MAX_SPEED 6.28
 
 // Percentual de velocidade que será
-// aplicado aos motores
-#define MOTOR_INTENSITY 0.9
+// aplicado aos motores nas curvas
+#define MOTOR_INTENSITY 0.6
 
-#define FORWARD_INTENSITY 0.7
+// Percentual de velocidade que será
+// aplicado aos motores nas retas
+#define FORWARD_INTENSITY 0.5
 
 #define IR_THRESHOLD 0.1
 
+// Valor que identifica a linha preta
 #define IR_TRACK_VALUE 5
 
 // Motores do E-Puck
@@ -43,9 +46,9 @@ double ps_values[8];
 
 // Sensores IR (infra-vermelho) para detectar
 // a linha do percurso.
-static WbDeviceTag left_ground, right_ground, rg_back, lg_back;
+static WbDeviceTag left_ground, right_ground;
 // Leitura da saída dos sensores
-static double left_ground_value, right_ground_value, rg_back_value, lg_back_value;
+static double left_ground_value, right_ground_value;
 
 // Tempo da simulação
 int time_step;
@@ -155,12 +158,6 @@ void init(){
   wb_distance_sensor_enable(left_ground, time_step);
   right_ground = wb_robot_get_device("IR1");
   wb_distance_sensor_enable(right_ground,time_step);
-   // Inicializa os sensores de IR do solo.
-   
-  lg_back = wb_robot_get_device("IR00");
-  wb_distance_sensor_enable(lg_back, time_step);
-  rg_back = wb_robot_get_device("IR11");
-  wb_distance_sensor_enable(rg_back,time_step);
   
 }
 
@@ -176,37 +173,23 @@ int main(int argc, char **argv) {
     left_ground_value = wb_distance_sensor_get_value(left_ground);
     right_ground_value = wb_distance_sensor_get_value(right_ground);
     
-    lg_back_value = wb_distance_sensor_get_value(lg_back);
-    rg_back_value = wb_distance_sensor_get_value(rg_back);
-    
     printf("\nGround Sensors Values: \n(Left)%4.4f\n(Right)%4.4f\n",
     left_ground_value,
     right_ground_value);
-    
-    printf("\nBack Ground Sensors Values: \n(Left)%4.4f\n(Right)%4.4f\n",
-    lg_back_value,
-    rg_back_value);
-    
-    /*if ( 
-     (left_ground_value >= 3 && left_ground_value <= 5) &&
-     (right_ground_value >= 3 && right_ground_value <= 5)
-     )
+   
+    motor_move_forward();    
+  
+    if( (left_ground_value > right_ground_value + 0.5) &&
+        (IR_TRACK_VALUE - IR_THRESHOLD < left_ground_value && left_ground_value < 150)){
+      printf("\nVirando Esquerda\n");
       motor_rotate_left();
-    else{  */
-      motor_move_forward();    
-    
-      if( (left_ground_value > right_ground_value + 0.5) &&
-          (IR_TRACK_VALUE - IR_THRESHOLD < left_ground_value && left_ground_value < 15)){
-        printf("\nVirando Esquerda\n");
-        motor_rotate_left();
-       }
-       else if( (right_ground_value > left_ground_value + 0.5) &&
-          (IR_TRACK_VALUE - IR_THRESHOLD < right_ground_value && left_ground_value < 15)){
-        printf("\nVirando Direita\n");
-        motor_rotate_right();
-       }
-       //}
-
+     }
+     else if( (right_ground_value > left_ground_value + 0.5) &&
+        (IR_TRACK_VALUE - IR_THRESHOLD < right_ground_value && right_ground_value < 150)){
+      printf("\nVirando Direita\n");
+      motor_rotate_right();
+     }
+     
     step();
   }
 }
